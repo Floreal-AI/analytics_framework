2b0358145b4d9c9fb1fad135ca0be60019648df9
Benjamin Gabay <bengabay@gmail.com>
2025-04-21 16:40:28 +0200

    Update neuron.py

--- quote_prediction_subnet/base/neuron.py
+++ quote_prediction_subnet/base/neuron.py
@@ -1,3 +1,20 @@
+# The MIT License (MIT)
+# Copyright © 2023 Yuma Rao
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the “Software”), to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+# the Software.
+
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+# THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
 import copy
 import typing
 
@@ -6,9 +23,9 @@ import bittensor as bt
 from abc import ABC, abstractmethod
 
 # Sync calls set weights and also resyncs the metagraph.
-from quote_prediction_subnet.utils.config import check_config, add_args, config
-from quote_prediction_subnet.utils.misc import ttl_get_block
-from quote_prediction_subnet import __spec_version__ as spec_version
+from conversion_subnet.utils.config import check_config, add_args, config
+from conversion_subnet.utils.misc import ttl_get_block
+from conversion_subnet import __spec_version__ as spec_version
 
 
 class BaseNeuron(ABC):
